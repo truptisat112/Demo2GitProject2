d81f2163056a1e11046a3257e8278be01c566288
truptisat112 <truptisat112@gmail.com>
2025-12-31 16:05:44 +0530

    Update Demo1.java

--- GitProject2/src/GitDemo1/Demo1.java
+++ GitProject2/src/GitDemo1/Demo1.java
@@ -2,24 +2,12 @@ package GitDemo1;
 
 public class Demo1 {
 	
-	public void m1() {
-		System.out.println("This is a m1 method");
-	}
-	
-	public void m2() {
-		System.out.println("This is a m2 method");
-	}
-		public void m3() {
-		System.out.println("This is a m3 method");
-	}
-
-	public void m4() {
-		System.out.println("This is a m4 method");
-	}
+System.out.println("This a main method for Demo class");
  public static void main(String args[]) {
 	 System.out.println("This a main method for Demo class");
  }
 }
+
 
 
 
