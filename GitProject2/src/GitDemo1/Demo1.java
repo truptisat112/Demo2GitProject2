bd91a3bd446c0a61e39265cac13d6b7b7054f50c
truptisat112 <truptisat112@gmail.com>
2025-12-31 15:48:58 +0530

    branch3- commit
    Added new m4 method

--- GitProject2/src/GitDemo1/Demo1.java
+++ GitProject2/src/GitDemo1/Demo1.java
@@ -12,10 +12,15 @@ public class Demo1 {
 		public void m3() {
 		System.out.println("This is a m3 method");
 	}
+
+	public void m4() {
+		System.out.println("This is a m4 method");
+	}
  public static void main(String args[]) {
 	 System.out.println("This a main method for Demo class");
  }
 }
+
 
 
 
