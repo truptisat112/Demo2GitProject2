40b9cc887d7f101aab2cec7b0453eb44f02265ae
LENOVO <LENOVO@DESKTOP-O28KSIB>
2025-12-31 15:36:20 +0530

    Commited  by trupti  for GitProject2

---
+++ GitProject2/src/GitDemo1/Demo1.java
@@ -0,0 +1,17 @@
+package GitDemo1;
+
+public class Demo1 {
+	
+	public void m1() {
+		System.out.println("This is a m1 method");
+	}
+	
+	
+	
+ public static void main(String args[]) {
+	 System.out.println("This a main method for Demo class");
+ }
+}
+
+
+
