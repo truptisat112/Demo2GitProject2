aa0cc80218a34ebe56750a145b510a600bf9a453
truptisat112 <truptisat112@gmail.com>
2025-12-31 15:41:25 +0530

    commit for GitProject2
    added m3 method

--- GitProject2/src/GitDemo1/Demo1.java
+++ GitProject2/src/GitDemo1/Demo1.java
@@ -9,11 +9,14 @@ public class Demo1 {
 	public void m2() {
 		System.out.println("This is a m2 method");
 	}
-	
+		public void m3() {
+		System.out.println("This is a m3 method");
+	}
  public static void main(String args[]) {
 	 System.out.println("This a main method for Demo class");
  }
 }
+
 
 
 
