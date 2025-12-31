df28dd21596661e40fd6a7a5bea857c959474327
truptisat112 <truptisat112@gmail.com>
2025-12-31 16:46:19 +0530

    b3
    2 constructor is present

--- GitProject2/src/GitDemo1/Demo1.java
+++ GitProject2/src/GitDemo1/Demo1.java
@@ -7,11 +7,14 @@ public class Demo1 {
 		public Demo1(){
 			
 		}
-
+	public Demo2(){
+			
+		}
  public static void main(String args[]) {
 	 System.out.println("This a main method for Demo class");
  }
 }
+
 
 
 
