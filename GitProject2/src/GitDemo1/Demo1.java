6f50965c02a1eae6d239d9004676b302ec1414ab
truptisat112 <truptisat112@gmail.com>
2025-12-31 16:36:42 +0530

    b2
    Added one constructor

--- GitProject2/src/GitDemo1/Demo1.java
+++ GitProject2/src/GitDemo1/Demo1.java
@@ -4,11 +4,15 @@ public class Demo1 {
 	public void m1(){
 		System.out.println("This a main method for Demo class");
 	}
+		public Demo1(){
+			
+		}
 
  public static void main(String args[]) {
 	 System.out.println("This a main method for Demo class");
  }
 }
+
 
 
 
